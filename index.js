7df4de290fd964064fe7cf4c87d24d6b625eeb78
Eric Whitcomb <eric.whitcomb@gmail.com>
2019-01-17 23:51:04 -0700

    Completed Day 2

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const db = require('./database/dbHelpers.js');
 
 const server = express();
 
+function protect(req, res, next) {
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.status(400).send('access denied');
+  }
+}
+
 server.use(express.json());
 server.use(cors());
+server.use(session({
+    name: 'notsession', // default is connect.sid
+    secret: 'nobody tosses a dwarf!',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000
+    }, // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: false,
+}));
 
 server.get('/', (req, res) => {
   res.send('Its Alive!');
@@ -30,6 +49,7 @@ server.post('/api/login', (req, res) => {
   db.findByUsername(user.username)
   .then(users => {
     if (users.length && bcrypt.compareSync(user.password, users[0].password)) {
+      req.session.userId = users[0].id;
       res.json({info: "correct"});
     } else {
       res.status(404).json({err: "Invalid username or password"}) ;
@@ -40,14 +60,23 @@ server.post('/api/login', (req, res) => {
   });
 });
 
+server.get('/api/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      res.status(500).send({err: 'Failed to logout'});
+    } else {
+      res.send({info: 'Logout successful'});
+    }
+  });
+});
+
 // protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
-  db('users')
-    .select('id', 'username')
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+server.get('/api/users', protect, (req, res) => {
+    db.getUsers()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
 });
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
